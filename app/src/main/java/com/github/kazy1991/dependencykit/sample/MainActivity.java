7c440a6317ba27ffbc579310cb398e655026021e
kazuki-yoshida <kzk.yshd@gmail.com>
2016-12-28 22:26:56 +0900

    Create DependencyKit module

--- app/src/main/java/com/github/kazy1991/dependencykit/sample/MainActivity.java
+++ app/src/main/java/com/github/kazy1991/dependencykit/sample/MainActivity.java
@@ -1,7 +1,9 @@
-package com.github.kazy1991.dependencykit;
+package com.github.kazy1991.dependencykit.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.kazy1991.dependencykit.R;
 
 public class MainActivity extends AppCompatActivity {
 
