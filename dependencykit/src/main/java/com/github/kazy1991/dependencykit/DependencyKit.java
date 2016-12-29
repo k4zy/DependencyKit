53b31861dd1d2986bc8e5640d61423b01c071288
kazuki-yoshida <kzk.yshd@gmail.com>
2016-12-29 23:19:32 +0900

    Implement hasTargetAnnotation instead of getDeclaredAnnotation

--- dependencykit/src/main/java/com/github/kazy1991/dependencykit/DependencyKit.java
+++ dependencykit/src/main/java/com/github/kazy1991/dependencykit/DependencyKit.java
@@ -1,6 +1,7 @@
 package com.github.kazy1991.dependencykit;
 
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -23,7 +24,7 @@ public class DependencyKit {
     public static void inject(Object instance) {
         final Field[] fields = instance.getClass().getDeclaredFields();
         for (Field field : fields) {
-            if (field.getDeclaredAnnotation(Inject.class) != null) {
+            if (hasTargetAnnotation(field.getDeclaredAnnotations(), Inject.class)) {
                 try {
                     field.setAccessible(true);
                     field.set(instance, get(field.getType()));
@@ -34,15 +35,28 @@ public class DependencyKit {
         }
     }
 
-    @SuppressWarnings("Since15")
+    private static boolean hasTargetAnnotation(Annotation[] annotations, Class<? extends Annotation> target) {
+        for (Annotation annotation : annotations) {
+            if (annotation.annotationType().equals(target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @SuppressWarnings("TryWithIdenticalCatches")
     private static Object get(Class<?> type) {
         Class<?> implType = rootBindingMap.get(type);
         for (Constructor constructor : implType.getDeclaredConstructors()) {
-            if (constructor.getDeclaredAnnotation(Inject.class) != null) {
+            if (hasTargetAnnotation(constructor.getDeclaredAnnotations(), Inject.class)) {
                 Object[] args = new Object[0];
                 try {
                     return constructor.newInstance(args);
-                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                } catch (InstantiationException e) {
+                    throw new IllegalStateException("Could not create instance");
+                } catch (IllegalAccessException e) {
+                    throw new IllegalStateException("Could not create instance");
+                } catch (InvocationTargetException e) {
                     throw new IllegalStateException("Could not create instance");
                 }
             }
